5cb43ccf5dd8790d561bd00a8041437d4140ab5f
Bram van Osta <bramvanosta@gmail.com>
2019-11-24 16:44:18 +0100

    refacto: All exception messages are now french

--- src/app/Exceptions/InvalidArgumentException.ts
+++ src/app/Exceptions/InvalidArgumentException.ts
@@ -2,7 +2,7 @@ import { HttpException } from '@adonisjs/generic-exceptions';
 
 export default class InvalidArgumentException extends HttpException {
   static get defaultMessage() {
-    return 'Tous les arguments nécessaires n\'ont pas été fournis.';
+    return "Tous les arguments nécessaires n'ont pas été fournis.";
   }
 
   constructor(message?: string | { [key: string]: string }) {
