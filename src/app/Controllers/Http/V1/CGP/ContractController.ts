b86ea27079c6dd7d68e66fb5c0aaa22ef4374d34
rfnadir <nadir.boufadene@robinfinance.com>
2019-11-25 16:12:02 +0100

    feature: added container tasks to contract api

--- src/app/Controllers/Http/V1/CGP/ContractController.ts
+++ src/app/Controllers/Http/V1/CGP/ContractController.ts
@@ -1,4 +1,5 @@
 import { Filters, Pagination } from '@robinfinance/js-api';
+import { Contract } from '../../../../Models/Contract';
 import { Context } from '../../../../../types';
 
 class CGPContractController {
@@ -15,9 +16,16 @@ class CGPContractController {
     const pagination = request.input('pagination') as Pagination;
     let filters = request.input('filters') as Filters;
     if ((filters && !('users' in filters)) || !filters) {
-      filters = { users: customer };
+      filters = { users: customer, ...filters };
     }
-    const contracts = await backendApi.findCGPContracts(pagination, filters);
+    let contracts: any = await backendApi.findCGPContracts(pagination, filters);
+
+    contracts = await Promise.all(
+      contracts.results.map(async (item: Contract) => {
+        const tasks = await backendApi.findCGPContractTasks({ page: 1, perPage: 100 }, { contract: item.getId() });
+        return { ...item, tasks: tasks.results };
+      }),
+    );
 
     response.status(200).send(contracts);
   }
