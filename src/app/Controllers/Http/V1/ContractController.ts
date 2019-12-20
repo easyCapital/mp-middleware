9d2b10e7251de316f7b1b16e13faaf46e27c8109
Bram van Osta <bramvanosta@gmail.com>
2019-12-20 16:01:39 +0100

    fix: Remove filter which hides contracts without a risk advice

--- src/app/Controllers/Http/V1/ContractController.ts
+++ src/app/Controllers/Http/V1/ContractController.ts
@@ -6,11 +6,6 @@ class ContractController {
   public async index({ request, response, backendApi }: Context) {
     const filters = request.input('filters', {}) as Filters;
 
-    // this would be more explicit with:
-    // filters.roles__user__proposition__risk_advice__isnull = false
-    // but it doesn't work (see https://robinfinance.atlassian.net/browse/RDB-3488)
-    filters._roles__user__proposition__risk_advice__isnull = 1;
-
     const contracts = await backendApi.getContracts(filters);
 
     response.status(200).send(contracts);
