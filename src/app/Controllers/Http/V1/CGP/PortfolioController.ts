1212187f759e2d23c47916d3c405c33f03637512
Bram van Osta <bramvanosta@gmail.com>
2020-03-05 15:16:09 +0100

    feature: RDB-3976: Add an API to search portfolios

--- src/app/Controllers/Http/V1/CGP/PortfolioController.ts
+++ src/app/Controllers/Http/V1/CGP/PortfolioController.ts
@@ -1,6 +1,17 @@
+import { Filters, Pagination } from '@robinfinance/js-api';
+
 import { Context } from '../../../../../types';
 
 class CGPPortfolioController {
+  public async index({ request, response, backendApi }: Context) {
+    const filters = request.input('filters') as Filters;
+    const pagination = request.input('pagination') as Pagination;
+
+    const portfolios = await backendApi.searchCGPPortfolios(pagination, filters);
+
+    response.status(200).send(portfolios);
+  }
+
   public async prevalidate({ request, response, backendApi }: Context) {
     const portfolio = request.post() as any;
 
