7b4fbf9bb8ffdfd31053131522060c3217c7d6a6
Bram van Osta <bramvanosta@gmail.com>
2020-03-09 10:39:55 +0100

    fix: Do not send proposition by mail if the investment value is higher than 500000

--- src/app/Listeners/Mieuxplacer/onPropositionGeneration.ts
+++ src/app/Listeners/Mieuxplacer/onPropositionGeneration.ts
@@ -1,13 +1,24 @@
 import { Context } from '../../../types';
+import { Proposition } from '../../Models/Proposition';
 
-async function onPropositionGeneration(context: Context, token: string) {
+async function onPropositionGeneration(context: Context, proposition: Proposition) {
   const { session, authenticated, symfonyApi } = context;
 
   if (!authenticated) {
-    session.put('lastPropositionToken', token);
+    session.put('lastPropositionToken', proposition.getToken());
   }
 
-  symfonyApi.sendPropositionByEmail(token);
+  let investmentAmount: string | undefined;
+
+  proposition.getAnswers().forEach(answer => {
+    if (answer.getKey() === 'sub_investment_amount1') {
+      investmentAmount = answer.getValue() as string;
+    }
+  });
+
+  if (proposition.getPortfolios().length > 0 && investmentAmount && Number(investmentAmount) < 500000) {
+    symfonyApi.sendPropositionByEmail(proposition.getToken());
+  }
 }
 
 export default onPropositionGeneration;
