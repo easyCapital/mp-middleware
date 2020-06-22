0cb2263f10610a0ada70ea89f41bbd4eecbf9ef4
Bram van Osta <bramvanosta@gmail.com>
2020-06-22 16:28:54 +0200

    fix: Add reverse observation category mapping before creation

--- src/app/Api/Backend/CGP/Observation/createObservation.ts
+++ src/app/Api/Backend/CGP/Observation/createObservation.ts
@@ -1,6 +1,7 @@
 import { ObservationDTO } from '@robinfinance/js-api';
 
 import { Observation } from '../../../../Models/Observation';
+import { ObservationCategoryMapper } from '../../../../Mappers/Observation';
 import { Exception } from '../../../../Exceptions';
 import { BackendException } from '../../Exceptions';
 import BackendApi from '../..';
@@ -11,12 +12,17 @@ export default async function createObservation(
   study: string | number,
   observation: ObservationDTO,
 ): Promise<Observation> {
+  const formattedObservation = {
+    ...observation,
+    category: observation.category && ObservationCategoryMapper.reverseTransform(observation.category),
+  };
+
   try {
     const response = await this.backendClient.post(
       {
         url: `cgp/customer/${customer}/study/${study}/observation/create`,
       },
-      observation,
+      formattedObservation,
     );
 
     const data = await response.json();
