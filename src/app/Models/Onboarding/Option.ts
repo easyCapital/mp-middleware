99831d88e93612e8788e8c075de7673830085ccb
Bram van Osta <bramvanosta@gmail.com>
2020-07-07 13:01:56 +0200

    feature: RDB-4877: Add annotation to Questions API

--- src/app/Models/Onboarding/Option.ts
+++ src/app/Models/Onboarding/Option.ts
@@ -7,12 +7,14 @@ interface OptionInterface {
 export default class Option implements OptionInterface {
   public value: string;
   public label: string;
+  private annotation?: string;
   private exclusive: boolean;
   private data?: any;
 
   constructor(json: any) {
     this.value = json.value;
     this.label = json.label;
+    this.annotation = json.annotation;
     this.exclusive = json.exclusive;
     this.data = json.data;
   }
@@ -20,8 +22,9 @@ export default class Option implements OptionInterface {
   public toJSON(): JsonOptionInterface {
     const json: JsonOptionInterface = {
       value: this.value,
-      exclusive: this.exclusive,
       label: this.label,
+      annotation: this.annotation,
+      exclusive: this.exclusive,
     };
 
     if (this.data) {
