1c75c622ab120430bf4741866e0d54d52a6894a8
Bram van Osta <bramvanosta@gmail.com>
2020-11-02 15:25:40 +0100

    feature: RDB-5404: Add ClientType filter to FAQ and Tutorials APIs (#102)

--- src/app/Api/Prismic/FAQ/getFAQ.ts
+++ src/app/Api/Prismic/FAQ/getFAQ.ts
@@ -1,16 +1,34 @@
-import { ContentTypes } from '@robinfinance/js-api';
+import { ClientTypes, ContentTypes, Filters } from '@robinfinance/js-api';
 
 import { FAQ } from '../../../Models/Prismic';
 import { NotFoundException } from '../../../Mappers/Exceptions';
 import { getAll } from '..';
 
 export default async function getFAQ(
-  filters?: { [filter: string]: string | string[] },
+  filters?: Filters,
   linked?: { [key: string]: string | string[] },
   fields?: string | string[],
   orderBy?: string,
 ): Promise<FAQ[]> {
-  const response = await getAll(ContentTypes.FAQ, filters, linked, fields, orderBy);
+  let formattedFilters: Filters = {};
+
+  if (filters) {
+    if ('type' in filters) {
+      if (filters.type === ClientTypes.CGP) {
+        formattedFilters.show_cgp = 'Oui';
+      } else if (filters.type === ClientTypes.COURTIER) {
+        formattedFilters.show_courtier = 'Oui';
+      } else if (filters.type === ClientTypes.ASSET_MANAGER) {
+        formattedFilters.show_asset_manager = 'Oui';
+      }
+
+      delete filters.type;
+    }
+
+    formattedFilters = { ...formattedFilters, ...filters };
+  }
+
+  const response = await getAll(ContentTypes.FAQ, formattedFilters, linked, fields, orderBy);
 
   if (response.length > 0) {
     const questions: FAQ[] = [];
--- src/app/Api/Prismic/Tutorial/getTutorials.ts
+++ src/app/Api/Prismic/Tutorial/getTutorials.ts
@@ -1,16 +1,34 @@
-import { ContentTypes } from '@robinfinance/js-api';
+import { ClientTypes, ContentTypes, Filters } from '@robinfinance/js-api';
 
 import { Tutorial } from '../../../Models/Prismic';
 import { NotFoundException } from '../../../Mappers/Exceptions';
 import { getAll } from '..';
 
 export default async function getTutorials(
-  filters?: { [filter: string]: string | string[] },
+  filters?: Filters,
   linked?: { [key: string]: string | string[] },
   fields?: string | string[],
   orderBy?: string,
 ): Promise<Tutorial[]> {
-  const response = await getAll(ContentTypes.TUTORIAL, filters, linked, fields, orderBy);
+  let formattedFilters: Filters = {};
+
+  if (filters) {
+    if ('type' in filters) {
+      if (filters.type === ClientTypes.CGP) {
+        formattedFilters.show_cgp = 'Oui';
+      } else if (filters.type === ClientTypes.COURTIER) {
+        formattedFilters.show_courtier = 'Oui';
+      } else if (filters.type === ClientTypes.ASSET_MANAGER) {
+        formattedFilters.show_asset_manager = 'Oui';
+      }
+
+      delete filters.type;
+    }
+
+    formattedFilters = { ...formattedFilters, ...filters };
+  }
+
+  const response = await getAll(ContentTypes.TUTORIAL, formattedFilters, linked, fields, orderBy);
 
   if (response.length > 0) {
     const tutorials: Tutorial[] = [];
