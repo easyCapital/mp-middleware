162ddc5bd170b3efd97ce48e0ce4df455bfdfdc1
Bram van Osta <bramvanosta@gmail.com>
2021-01-26 19:16:30 +0100

    hotfix: Fetch all answers event when list is higher than 100

--- src/app/Api/Backend/CGP/Answer/getCGPAnswers.ts
+++ src/app/Api/Backend/CGP/Answer/getCGPAnswers.ts
@@ -3,6 +3,7 @@ import { Filters } from '@robinfinance/js-api';
 import { Answer } from '../../../../Models/Answer';
 import { Exception } from '../../../../Exceptions';
 import { BackendException } from '../../Exceptions';
+import { formatMeta } from '../../Helpers';
 import BackendApi from '../..';
 
 export default async function getCGPAnswers(this: BackendApi, filters?: Filters, latestBy?: string): Promise<Answer[]> {
@@ -15,6 +16,33 @@ export default async function getCGPAnswers(this: BackendApi, filters?: Filters,
     const data = await response.json();
 
     const answers = data.map((item) => new Answer(item));
+    const meta = formatMeta(response.headers);
+
+    if (meta.totalPages && meta.totalPages > 1) {
+      const answerQueries: Promise<any>[] = [];
+
+      for (let page = 2; page <= meta.totalPages; page += 1) {
+        answerQueries.push(
+          this.backendClient.get({
+            url: 'cgp/answer/search',
+            filters,
+            latestBy,
+            pagination: {
+              page,
+              perPage: 100,
+            },
+          }),
+        );
+      }
+
+      const answerResponses = await Promise.all(answerQueries);
+
+      for await (const answerResponse of answerResponses) {
+        const answerData = await answerResponse.json();
+
+        answerData.forEach((item) => answers.push(new Answer(item)));
+      }
+    }
 
     return answers;
   } catch (exception) {
--- src/app/Api/Backend/CGP/Answer/getCustomerAnswers.ts
+++ src/app/Api/Backend/CGP/Answer/getCustomerAnswers.ts
@@ -3,6 +3,7 @@ import { Filters } from '@robinfinance/js-api';
 import { Answer } from '../../../../Models/Answer';
 import { Exception } from '../../../../Exceptions';
 import { BackendException } from '../../Exceptions';
+import { formatMeta } from '../../Helpers';
 import BackendApi from '../..';
 
 export default async function getCustomerAnswers(
@@ -11,15 +12,44 @@ export default async function getCustomerAnswers(
   filters?: Filters,
   latestBy?: string,
 ): Promise<Answer[]> {
+  const formattedFilters = filters ? { ...filters, user: customerId } : { user: customerId };
+
   try {
     const response = await this.backendClient.get({
       url: 'cgp/answer/search',
-      filters: filters ? { ...filters, user: customerId } : { user: customerId },
+      filters: formattedFilters,
       latestBy,
     });
     const data = await response.json();
 
     const answers = data.map((item) => new Answer(item));
+    const meta = formatMeta(response.headers);
+
+    if (meta.totalPages && meta.totalPages > 1) {
+      const answerQueries: Promise<any>[] = [];
+
+      for (let page = 2; page <= meta.totalPages; page += 1) {
+        answerQueries.push(
+          this.backendClient.get({
+            url: 'cgp/answer/search',
+            filters: formattedFilters,
+            latestBy,
+            pagination: {
+              page,
+              perPage: 100,
+            },
+          }),
+        );
+      }
+
+      const answerResponses = await Promise.all(answerQueries);
+
+      for await (const answerResponse of answerResponses) {
+        const answerData = await answerResponse.json();
+
+        answerData.forEach((item) => answers.push(new Answer(item)));
+      }
+    }
 
     return answers;
   } catch (exception) {
